d4189398376916787873a4bdbd78f0824289d0d1
mats-js <100519629+mats-js@users.noreply.github.com>
2022-10-05 10:22:27 +0200

    Added axios for ajax operations
    - Replaced in-memory movies array with GET method from backend

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import axios from 'axios';
+
 import MovieCard from '../movie-card/movie-card';
 import MovieView from '../movie-view/movie-view';
 
-// Import posters from local
+// Import posters from local (DELETE LATER!!!)
 import posterInception from '/inception.jpg';
 import posterShawshank from '/shawshank.jpg';
 import posterGladiator from '/gladiator.jpg';
@@ -11,29 +13,7 @@ export default class MainView extends React.Component {
   constructor() {
     super();
     this.state = {
-      movies: [
-        {
-          _id: 1,
-          Title: 'Inception',
-          Description:
-            'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.',
-          ImagePath: posterInception,
-        },
-        {
-          _id: 2,
-          Title: 'The Shawshank Redemption',
-          Description:
-            'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
-          ImagePath: posterShawshank,
-        },
-        {
-          _id: 3,
-          Title: 'Gladiator',
-          Description:
-            'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.',
-          ImagePath: posterGladiator,
-        },
-      ],
+      movies: [],
       selectedMovie: null,
     };
   }
@@ -47,8 +27,7 @@ export default class MainView extends React.Component {
   render() {
     const { movies, selectedMovie } = this.state;
 
-    if (movies.length === 0)
-      return <div className="main-view">The list is empty!</div>;
+    if (movies.length === 0) return <div className="main-view" />;
 
     return (
       <div className="main-view">
@@ -72,5 +51,18 @@ export default class MainView extends React.Component {
         )}
       </div>
     );
+  }
+
+  componentDidMount() {
+    axios
+      .get('https://mats-js-myflixdb.herokuapp.com/movies')
+      .then((res) => {
+        this.setState({
+          movies: res.data,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 }
